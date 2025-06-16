ddedc03573b17d402653d20f2255f0c3f73512a1
Rohit Khadka <rohitkhadka153@gmail.com>
2025-06-16 18:15:31 +0545

    fix: login credentials

--- Trading Bot/trading_bot.py
+++ Trading Bot/trading_bot.py
@@ -1,13 +1,15 @@
 import MetaTrader5 as mt5
 import time
+import os
 from mt5_trade_functions import market_order, close_all_positions
-
+from dotenv import load_dotenv
+load_dotenv()
 if __name__ == '__main__':
 
     # open your own trading account: https://icmarkets.com/?camp=60457
-    login = 51465418
-    password = 'JPFfsLBB'
-    server = 'ICMarketsEU-Demo'
+    login = os.environ.get("LOGIN")
+    password = os.environ.get("PASSWORD")
+    server = os.environ.get("SERVER")
 
     mt5.initialize()
     mt5.login(login, password, server)
